634cd83c85ec99e997e61742a01d767509123bb8
Talha Azhar <txa2279@rit.edu>
2018-08-31 14:45:35 -0400

    works for 3play

---
+++ content.js
@@ -0,0 +1,34 @@
+var container = document.body.getElementsByClassName('integration_name pull-left');
+var input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("id", "user");
+input.setAttribute("placeholder", "Search for users..");
+
+input.addEventListener('input', function (evt) {
+    updateUser(this.value);
+});
+
+container[0].appendChild(input);
+
+
+/**
+ * Looks through the options and change the selected option 
+ * in accordance to the input by user
+ * 
+ * @param {userID} the input from the user
+ */
+
+function updateUser(userID){
+    var select = document.getElementById('search_library_id');
+    userID = userID.toUpperCase();
+    var opts = select.options;
+    for (var opt, j = 0; opt = opts[j]; j++) {
+        if (opt.text.toUpperCase().indexOf(userID) > -1) {
+            select.selectedIndex = j;
+            break;
+        }
+    }
+}
+
+
+
